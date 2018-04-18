4455e48d4c656c374c78dc897eca68bc1a9cb232
Radhika Gupta <radhikgu@microsoft.com>
2018-04-18 14:51:35 -0700

    Fix the VHD to be exact version after discussion with the team.

--- pkg/acsengine/defaults.go
+++ pkg/acsengine/defaults.go
@@ -195,7 +195,7 @@ var (
 				ImageOffer:     "UbuntuServer",
 				ImageSku:       "16.04-LTS",
 				ImagePublisher: "Canonical",
-				ImageVersion:   "latest",
+				ImageVersion:   "16.04.201802220",
 			},
 			api.RHEL: DefaultRHELOSImageConfig,
 		},
